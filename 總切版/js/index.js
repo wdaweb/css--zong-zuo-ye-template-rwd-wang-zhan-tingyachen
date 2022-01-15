3d1d41a723801b92bc18271aaa41d5d781990d0a
tingyachen <tina931691221@gmail.com>
2022-01-15 16:09:51 +0800

    upload

---
+++ 總切版/js/index.js
@@ -0,0 +1,10 @@
+// burger menu
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '360px'
+  document.getElementById('main').style.marginLeft = '250px'
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0'
+  document.getElementById('main').style.marginLeft = '0'
+}
